21dcd9925c2009274a53c19e6ddd7378b3259b16
leo417 <leosab_2@hotmail.com>
2023-11-08 10:40:56 +0100

    version2

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ redactTypeSelect.addEventListener('change', redact);
 // Function to begin redaction
 function redact() {
 
-    //Acquire message input
+    // Store message input
     const message = document.getElementById("message");
+    // Created redacted message output
+    var redactedMsg = document.getElementById("redactedMsg");
+    // Store words to be redacted
+    const redactionString= document.getElementById("redactStr").value;
 
     // If message is empty dispaly message, box error color change, end function 
     if (!message.value)  {
@@ -13,35 +17,19 @@ function redact() {
         alert("Enter your message in the message box");
         return;
     } 
+    // If there is a message but no words to redact, then return message and end function
+    else if  (message.value && !redactionString) {
+        redactedMsg.innerHTML = message.value;
+        return; 
+    }
     
     // Message input box blue confirmation chaage 
     else {
         message.style.borderBlockColor = 'blue';
-    }
+        // Create array of words from redact words
+    arrayOfRedactWords = redactionString.split(',').map((word) => word.trim());
 
-    // Created redacted mesage output
-    var redactedMsg = document.getElementById("redactedMsg");
-    // Acquire words to be redacted
-    const redactionString= document.getElementById("redactStr");
-
-    // Edit message if redact words, else dispaly the exact message 
-    !redactionString.value ? redactedMsg.innerHTML = message.value : redactedMsg.innerHTML = redactFxn(message.value, redactionString.value, redactTypeSelect.value);
-
-}
-
-// Actual redaction function logic
-function redactFxn(msg, redactString, type ) {
-
-    if (type == 'none' ) {
-        return message.value;
-    }
-
-    let tempMsg = msg;
-
-    // Create array of words from redact words
-    arrayOfRedactWords = redactString.split(',').map((word) => word.trim());
-
-    // Loop through redact words and modify the input message, redacting where necessary
+     // Loop through redact words and modify the input message, redacting where necessary
     for (word of arrayOfRedactWords) {
         console.log(word);
         
@@ -49,17 +37,20 @@ function redactFxn(msg, redactString, type ) {
         var regex = new RegExp(`(?:[".(_*-]|\\b)${word}(?:[".(_*-]|\\b)`, "gim");
         
         // Blur replacemenet with a span
-        if (type == 'blur') {
-            tempMsg = tempMsg.replaceAll(regex, `<span id='filter'>${word}</span>`);
+        if (redactTypeSelect.value == 'blur') {
+            redactedMsg.innerHTML = message.value.replaceAll(regex, `<span id='filter'>${word}</span>`);
         } 
-        else if (!type) {
+        else if (redactTypeSelect.value == 'none') {
+            redactedMsg.innerHTML = message.value;
+        } 
+        else if (!redactTypeSelect.value) {
             return ;
         }
         // All other types of redact methods
         else {
-            console.log(type);
-            tempMsg = tempMsg.replaceAll(regex, type);
+            redactedMsg.innerHTML = message.value.replaceAll(regex, redactTypeSelect.value);            
         }
     }
+}
     return ;
 }
