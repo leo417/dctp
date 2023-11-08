e6d8aab49cfaec5e3c02ca63366e1105a86628b6
leo417 <leosab_2@hotmail.com>
2023-11-08 09:53:13 +0100

    re removed unnecessary stuff

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const redactOptions = {"ellipsis": '...', "questions": '???', "dashes": '---', "underscores": '___', "blur":'', "asterisk": '***'};
 const redactTypeSelect= document.getElementById("redactType");
 redactTypeSelect.addEventListener('change', redact);
-var arrayOfRedactWords =[];
-var tempMsg;
 
 // Function to begin redaction
 function redact() {
@@ -26,9 +23,6 @@ function redact() {
     var redactedMsg = document.getElementById("redactedMsg");
     // Acquire words to be redacted
     const redactionString= document.getElementById("redactStr");
-    
-    // Select redact type value
-    // let redactType= document.getElementById("redactType").value;
 
     // Edit message if redact words, else dispaly the exact message 
     !redactionString.value ? redactedMsg.innerHTML = message.value : redactedMsg.innerHTML = redactFxn(message.value, redactionString.value, redactTypeSelect.value);
@@ -67,72 +61,5 @@ function redactFxn(msg, redactString, type ) {
             tempMsg = tempMsg.replaceAll(regex, type);
         }
     }
-
-    console.log(tempMsg);
-
-    return tempMsg
+    return ;
 }
-
-
-
-// function findCensoredArray(censorString) {
-//     console.log("This is censroString", censorString.value);
-// inputLength = censorString.value.trim();
-// const censorWords = censorString.value.trim().split();
-// const listArrays = censorWords.map((word) => [...msg.toLowerCase().matchAll(word)]);
-// // console.log(listArrays);
-// // l= listArrays;
-// msgList =listArrays[0].map((obj) => [obj['index'], obj['index']+ obj[0].length]);
-// console.log(msgList);
-// l= msgList;
-
-//     return msgList;
-// }
-
-// function remainderParagraph(arr) {
-//     const start = 0;
-//     final = [];
-
-//     for (let i =0; i < arr.length; i++) {
-//         if (i == 0 && arr[i][0] != 0 && arr[i][1] != inputLength - 1 ) {
-//             final.append([start, arr[i][0] - start]); 
-//             final.append([arr[i][0], arr[i][1], 'span' ])
-//             start  = arr[i][1] + 1;           
-//         }
-//         else if (i == 0 && arr[i][0] == 0) {
-//             final.append([start, arr[i][0], 'span']);
-//             start = arr[i][1] + 1;
-//         }
-
-
-//         else if (arr[i][1] == inputLength - 1) {
-//             final.append([arr[i][0], arr[i][1] ]);
-//             return final;
-//         }
-//         else if (arr[i-1][1] == arr[i][0] ) {
-//             final.append([arr[i][0], arr[i][1]  ])
-//             start  = arr[i][1] + 1; 
-//         }
-//         else {
-//             final.append([start, arr[i][0] - 1]);  
-//             start  = arr[i][0]; 
-//         }
-//     }
-//     arr.map((idx)=> {start, idx[1] - start;} );
-// }
-
-// function remainderIndices(arr) {
-//     // newArr = [start, arr[1] start]
-// }
-
-
-
-    // newMsg = arrayOfRedactWords.map((word) => {
-        
-    //     var regex = new RegExp(`(?:[".(_*-]|\\b)${word}(?:[".(_*-]|\\b)`, "gim");
-    //     tempMsg= arrayOfRedactWords.map((word) => msg.replaceAll(regex, redactOptions[type]));
-    // });
-
-    // console.log(tempMsg[0]);
-
-    // return tempMsg[0] ;
